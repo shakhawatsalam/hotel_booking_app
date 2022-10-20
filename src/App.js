efaff55885a29cfe7dcfae3f9500ee5f02060dbe
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-10-20 10:39:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import List from "./pages/list/List";
+import Home from "./pages/home/Home";
+import Hotels from "./pages/hotel/Hotels";
+
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotels/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
