59877d85a4ac3207c1ceb454b2c233b5060370da
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-10-24 14:13:04 +0600

    working  on mailList

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
@@ -16,7 +17,8 @@ const Home = () => {
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties/>
+        <FeaturedProperties />
+        <MailList/>
       </div>
     </div>
   );
