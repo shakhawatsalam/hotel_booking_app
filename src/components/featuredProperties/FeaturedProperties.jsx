3c4b72fea4a421a26a8647f5d3e59f8ea50ec40f
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-10-24 12:47:44 +0600

    featuredProperties section is almost done

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./featuredProperties.css";
+const FeaturedProperties = () => {
+  return (
+    <div className='fp'>
+      <div className='fpItem'>
+        <div className='fpImg'>
+          <img
+            src='https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+            alt=''
+          />
+        </div>
+        <span className='fpName'>Sea view Hotel</span>
+        <span className='fpCity'>Cox'z Bazar</span>
+        <span className='fpPrice'>Starting from 120$</span>
+        <div className='fpRating'>
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className='fpItem'>
+        <div className='fpImg'>
+          <img
+            src='https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+            alt=''
+          />
+        </div>
+        <span className='fpName'>Sea view Hotel</span>
+        <span className='fpCity'>Cox'z Bazar</span>
+        <span className='fpPrice'>Starting from 120$</span>
+        <div className='fpRating'>
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className='fpItem'>
+        <div className='fpImg'>
+          <img
+            src='https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+            alt=''
+          />
+        </div>
+        <span className='fpName'>Sea view Hotel</span>
+        <span className='fpCity'>Cox'z Bazar</span>
+        <span className='fpPrice'>Starting from 120$</span>
+        <div className='fpRating'>
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+      <div className='fpItem'>
+        <div className='fpImg'>
+          <img
+            src='https://images.unsplash.com/photo-1568084680786-a84f91d1153c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+            alt=''
+          />
+        </div>
+        <span className='fpName'>Sea view Hotel</span>
+        <span className='fpCity'>Cox'z Bazar</span>
+        <span className='fpPrice'>Starting from 120$</span>
+        <div className='fpRating'>
+          <button>8.9</button>
+          <span>Excellent</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
