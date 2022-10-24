51fc47543675f76f42905a5e05d8cb27bc0814ef
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-10-24 11:12:48 +0600

    property list section is almost done

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,50 +1,77 @@
 import React from "react";
-import './propertyList.css';
+import "./propertyList.css";
 const PropertyList = () => {
-    return (<div className="pList">
-        <div className="pListitem">
-            <img src="https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80" alt="" />
-            <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233, hotels</h2>
-            </div>
-
+  return (
+    <div className='pList'>
+      <div className='pListItem'>
+        <div className='img'>
+          <img
+            src='https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80'
+            alt=''
+            className='pListImg'
+          />
         </div>
-        <div className="pListitem">
-            <img src="https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80" alt="" />
-            <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233, hotels</h2>
-            </div>
 
+        <div className='pListTitles'>
+          <h1>Hotels</h1>
+          <h2>233, hotels</h2>
         </div>
-        <div className="pListitem">
-            <img src="https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80" alt="" />
-            <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233, hotels</h2>
-            </div>
+      </div>
+      <div className='pListItem'>
+        <div className='img'>
+          <img
+            src='https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80'
+            alt=''
+            className='pListImg'
+          />
+        </div>
 
+        <div className='pListTitles'>
+          <h1>Hotels</h1>
+          <h2>233, hotels</h2>
         </div>
-        <div className="pListitem">
-            <img src="https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80" alt="" />
-            <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233, hotels</h2>
-            </div>
-
+      </div>
+      <div className='pListItem'>
+        <div className='img'>
+          <img
+            src='https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80'
+            alt=''
+            className='pListImg'
+          />
         </div>
-        <div className="pListitem">
-            <img src="https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80" alt="" />
-            <div className="pListTitles">
-                <h1>Hotels</h1>
-                <h2>233, hotels</h2>
-            </div>
-
+        <div className='pListTitles'>
+          <h1>Hotels</h1>
+          <h2>233, hotels</h2>
         </div>
+      </div>
+      <div className='pListItem'>
+        <div className='img'>
+          <img
+            src='https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80'
+            alt=''
+            className='pListImg'
+          />
+        </div>
+        <div className='pListTitles'>
+          <h1>Hotels</h1>
+          <h2>233, hotels</h2>
+        </div>
+      </div>
+      <div className='pListItem'>
+        <div className='img'>
+          <img
+            src='https://images.unsplash.com/photo-1482192505345-5655af888cc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1228&q=80'
+            alt=''
+            className='pListImg'
+          />
+        </div>
+        <div className='pListTitles'>
+          <h1>Hotels</h1>
+          <h2>233, hotels</h2>
+        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default PropertyList;
-
