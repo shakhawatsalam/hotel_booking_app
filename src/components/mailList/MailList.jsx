8833fc624424921524a80b915ddfe822a636cd4b
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-10-24 15:28:27 +0600

    mailList is almost done

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import "./mailList.css";
+import video from "../../Assets/swi.mp4";
 const MailList = () => {
   return (
     <div className='mail'>
-      <h1 className='mailTitle'>Save time, save money!</h1>
+      <div className='mailOverlay'></div>
+      <video
+        className='mailVideo'
+        src={video}
+        muted
+        autoPlay
+        loop
+        type='video/mp4'></video>
+      <h1 className='mailTitle'>“Jobs fill your pockets, adventures fill your soul.”</h1>
       <span className='mailDesc'>
         Sign up and we'll send the best deals to you
       </span>
       <div className='mailInputContainer'>
-        <input type='text' placeholder='Your Email' />
-        <button>Subscribe</button>
+        <div>
+          <input className="input" type='text' placeholder='Your Email' />
+        </div>
+        <div>
+          <button className="subButton">Subscribe</button>
+        </div>
       </div>
     </div>
   );
